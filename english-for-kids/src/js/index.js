f3b8d279e5946f02e46054f154171d222aedde33
Ihar Bachkouski <igorko.by@gmail.com>
2020-04-16 22:27:22 +0300

    feat: add handler to toggle menu

--- english-for-kids/src/js/index.js
+++ english-for-kids/src/js/index.js
@@ -1,1 +1,37 @@
+// import cards from './cards';
 
+const main = document.querySelector('main');
+const switcher = document.querySelector('.switch');
+const menu = document.querySelector('.menu');
+const menuItems = document.querySelectorAll('.menu__item');
+const btnMenu = document.querySelector('.menu-button');
+
+const handleCloseMenu = () => {
+  menu.classList.remove('menu--shown');
+  btnMenu.classList.remove('menu-button--active');
+  btnMenu.removeEventListener('click', handleCloseMenu);
+  main.removeEventListener('click', handleCloseMenu);
+  switcher.removeEventListener('click', handleCloseMenu);
+  menuItems.forEach((item) => {
+    item.removeEventListener('click', handleCloseMenu);
+  });
+};
+
+const handleOpenMenu = () => {
+  menu.classList.add('menu--shown');
+  btnMenu.classList.add('menu-button--active');
+  btnMenu.addEventListener('click', handleCloseMenu);
+  main.addEventListener('click', handleCloseMenu);
+  switcher.addEventListener('click', handleCloseMenu);
+  menuItems.forEach((item) => {
+    item.addEventListener('click', handleCloseMenu);
+  });
+};
+
+const handleMenu = () => {
+  btnMenu.addEventListener('click', handleOpenMenu);
+};
+
+window.onload = () => {
+  handleMenu();
+};
